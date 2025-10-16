4b0bd5d1704ff1b38ac55a2e7e3f14f3f93c1e57
D123gowda <dhanushgowda8899@gmail.com>
2025-10-16 09:57:01 +0530

    Add files via upload

---
+++ reactdemo/react/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+
+const  App = () => {
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <p>I am Dhanush , From AIML</p>
+    </div>
+  )
+}
+export default App;
